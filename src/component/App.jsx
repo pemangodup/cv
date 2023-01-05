0a74b61228863b20255bd46ad3f6b0fae8341b47
Pema Ngodup <pngodup123@gmail.com>
2023-01-05 11:35:26 +0000

    Added more design in about section

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -14,12 +14,21 @@ const App = () => {
   const toggle = () => {
     setStyle(!style);
   };
+
+  const personDetails = [
+    { h: 'Age', e: '28' },
+    { h: 'Email', e: 'pngodup123@getDefaultNormalizer.com' },
+    { h: 'Phone', e: '920090988' },
+    { h: 'Age', e: '28' },
+    { h: 'Email', e: 'pngodup123@getDefaultNormalizer.com' },
+    { h: 'Phone', e: '920090988' },
+  ];
   return (
     <div className="main-container">
       <section className="heading-container">
         {/* This is my top container with image and button */}
         <div className="top-header">
-          <a href="src">
+          <a href="#home">
             <img
               className="pro-img"
               src="https://media.licdn.com/dms/image/C5603AQHRv-dTiZ9xOA/profile-displayphoto-shrink_400_400/0/1625419846609?e=1678320000&v=beta&t=0mzdneY0jqO_lv2M3jAw7nZkooBEVj4iWzgju5iKlBw"
@@ -34,18 +43,42 @@ const App = () => {
         {/* this is my bottom container with list of menu items */}
         <div className="bottom-header">
           <ul className={style ? 'ul' : 'ul navactive'}>
-            <li className="li">About</li>
-            <li className="li">Services</li>
-            <li className="li">Skills</li>
-            <li className="li">Portfolio</li>
-            <li className="li">Experiences</li>
-            <li className="li">Contact</li>
+            <li className="li">
+              <a href="#about" className="li">
+                About
+              </a>
+            </li>
+            <li className="li">
+              <a href="#about" className="li">
+                Services
+              </a>
+            </li>
+            <li className="li">
+              <a href="#about" className="li">
+                Skills
+              </a>
+            </li>
+            <li className="li">
+              <a href="#about" className="li">
+                Portfolio
+              </a>
+            </li>
+            <li className="li">
+              <a href="#about" className="li">
+                Experiences
+              </a>
+            </li>
+            <li className="li">
+              <a href="#about" className="li">
+                Contact
+              </a>
+            </li>
           </ul>
         </div>
       </section>
       <section className="body-container">
         {/* FIRST CONTAINER INSIDE BODY SECTION "HOME CONTAINER" */}
-        <div className="home-container">
+        <div className="home-container" id="home">
           <div className="pic-container">
             <img
               className="picSize"
@@ -58,11 +91,36 @@ const App = () => {
             <div className="name-text">I,m Pema Ngodup</div>
             <div className="profesion-text">Web Developer</div>
             <div className="icons">
-              <FontAwesomeIcon className="icon" icon={faTwitter} />
-              <FontAwesomeIcon className="icon" icon={faFacebook} />
-              <FontAwesomeIcon className="icon" icon={faInstagram} />
-              <FontAwesomeIcon className="icon" icon={faLinkedin} />
-              <FontAwesomeIcon className="icon" icon={faBehance} />
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+              >
+                <FontAwesomeIcon className="icon" icon={faLinkedin} />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+              >
+                <FontAwesomeIcon className="icon" icon={faTwitter} />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+              >
+                <FontAwesomeIcon className="icon" icon={faFacebook} />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+              >
+                <FontAwesomeIcon className="icon" icon={faInstagram} />
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+              >
+                <FontAwesomeIcon className="icon" icon={faBehance} />
+              </a>
             </div>
             <button className="btn">
               Get In Touch <FontAwesomeIcon icon="fa-regular fa-arrow-right" />
@@ -70,7 +128,7 @@ const App = () => {
           </div>
         </div>
         {/* SECOND CONTAINER INSIDE BODY SECTION "ABOUT CONTAINER" */}
-        <div className="about-container">
+        <div className="about-container" id="about">
           <div className="about-container-detail">
             <div className="about-subject">Hello! I am Pema Ngodup</div>
             <div className="about-person">
@@ -80,9 +138,24 @@ const App = () => {
               gets the job done. I can easily capitalize on low hanging fruits
               and quickly maximize timely deliverables for real-time schemas.
             </div>
-            <div className="about-details"></div>
+            <div className="about-details">
+              {personDetails.map((deta) => {
+                return (
+                  <div className="straight-line">
+                    <div className="label">{deta.h}:</div>
+                    <div className="detail">{deta.e}</div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="about-container-image"></div>
+          <div className="about-image">
+            <img
+              className="responsive-pic"
+              src="https://media.licdn.com/dms/image/C5603AQHRv-dTiZ9xOA/profile-displayphoto-shrink_800_800/0/1625419846609?e=1678320000&v=beta&t=zwm8vLbwZgeuqHab66Cm6h75zXTug7Fyi08lHVFFs4E"
+              alt="profile.jpg"
+            />
+          </div>
         </div>
       </section>
       <section className="footer-container">I am footer.</section>
