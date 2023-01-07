dd869c0f18ad6e09fb168aaf7016b3c0939f264d
Pema Ngodup <pngodup123@gmail.com>
2023-01-07 10:48:28 +0000

    Footer component and css file created

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,17 +1,11 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Heading from './heading/Heading';
 import Home from './heading/Home';
 import About from './heading/About';
 import Experience from './heading/Experience';
 import Contact from './heading/Contact';
-import {
-  faTwitter,
-  faFacebook,
-  faInstagram,
-  faLinkedin,
-  faBehance,
-} from '@fortawesome/free-brands-svg-icons';
+import Footer from './heading/Footer';
+
 const App = () => {
   return (
     <div className="main-container">
@@ -23,48 +17,7 @@ const App = () => {
         <Contact />
       </section>
       {/* End of body section */}
-      <section className="footer-container">
-        <div className="footer-1">Pema Ngodup</div>
-        <div className="footer-2">Node JS & React JS Developer</div>
-        <div className="footer-3">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/pema-ngodup-529438130/"
-          >
-            <FontAwesomeIcon className="icon" icon={faLinkedin} />
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/pema-ngodup-529438130/"
-          >
-            <FontAwesomeIcon className="icon" icon={faTwitter} />
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/pema-ngodup-529438130/"
-          >
-            <FontAwesomeIcon className="icon" icon={faFacebook} />
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/pema-ngodup-529438130/"
-          >
-            <FontAwesomeIcon className="icon" icon={faInstagram} />
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/pema-ngodup-529438130/"
-          >
-            <FontAwesomeIcon className="icon" icon={faBehance} />
-          </a>
-        </div>
-        <div className="footer-4">&copy;Pema Ngoudp. All Rights Reserved</div>
-      </section>
+      <Footer />
     </div>
   );
 };
---
+++ src/component/heading/Footer.jsx
@@ -0,0 +1,56 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faBehance,
+} from '@fortawesome/free-brands-svg-icons';
+const Footer = () => {
+  return (
+    <section className="footer-container">
+      <div className="footer-1">Pema Ngodup</div>
+      <div className="footer-2">Node JS & React JS Developer</div>
+      <div className="footer-3">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+        >
+          <FontAwesomeIcon className="icon" icon={faLinkedin} />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+        >
+          <FontAwesomeIcon className="icon" icon={faTwitter} />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+        >
+          <FontAwesomeIcon className="icon" icon={faFacebook} />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+        >
+          <FontAwesomeIcon className="icon" icon={faInstagram} />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/pema-ngodup-529438130/"
+        >
+          <FontAwesomeIcon className="icon" icon={faBehance} />
+        </a>
+      </div>
+      <div className="footer-4">&copy;Pema Ngoudp. All Rights Reserved</div>
+    </section>
+  );
+};
+export default Footer;
