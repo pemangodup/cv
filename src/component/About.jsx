441531687b1f40d8a69b657b6c6abe0b4dce0291
Pema Ngodup <pngodup123@gmail.com>
2023-01-17 13:10:38 +0000

    Github deployment changes

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,4 +1,4 @@
-// import img from '../img/pema.jpg';
+import img from '../img/pema.jpg';
 import variable from '../config/config';
 import './About.css';
 import { Slide } from 'react-awesome-reveal';
@@ -39,8 +39,8 @@ function About() {
         <Slide direction="right">
           <img
             className="responsive-pic"
-            src="https://images.unsplash.com/photo-1597019558926-3eef445fdf60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWN0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-            // src={img}
+            // src="https://images.unsplash.com/photo-1597019558926-3eef445fdf60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWN0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            src={img}
             // src="https://media.licdn.com/dms/image/C5603AQHRv-dTiZ9xOA/profile-displayphoto-shrink_800_800/0/1625419846609?e=1678320000&v=beta&t=zwm8vLbwZgeuqHab66Cm6h75zXTug7Fyi08lHVFFs4E"
             alt="profile.jpg"
           />
