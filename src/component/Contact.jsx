79d5ebb904e07722988f4429477940a9640999be
Pema Ngodup <pngodup123@gmail.com>
2023-01-26 13:08:45 +0000

    Changes in position in the contact component

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -2,9 +2,13 @@ import './Contact.css';
 import { Slide } from 'react-awesome-reveal';
 import { useEffect, useState } from 'react';
 import variable from '../config/config';
+import { useRef } from 'react';
 
 function Contact() {
   const [contact, setContact] = useState([]);
+  let name = useRef('');
+  let email = useRef('');
+  let message = useRef('');
   useEffect(() => {
     fetchContact();
   }, []);
@@ -14,27 +18,29 @@ function Contact() {
     const contact = await response.json();
     setContact(contact.data);
   }
+
+  async function submitHandler() {
+    const api = variable.API_URI;
+    const postData = {
+      name: name.current.value,
+      email: email.current.value,
+      message: message.current.value,
+    };
+    const res = await fetch(`${api}/sendMail`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(postData),
+    });
+    if (!res.ok) {
+      const message = `An error occured: ${res.status} - ${res.statusText}`;
+      throw new Error(message);
+    }
+  }
   return (
     <div className="contact-container" id="contact">
       <div className="upper-contact-container">Contact Me</div>
       <div className="lower-contact-container">
-        <Slide direction="left" className="contact-form">
-          <form action="" method="post">
-            <label htmlFor="">Name</label>
-            <br />
-            <input type="text" />
-            <br />
-            <label htmlFor="">Email</label>
-            <br />
-            <input type="email" />
-            <br />
-            <label htmlFor="">Message</label>
-            <br />
-            <textarea name="" id="" cols="30" rows="12"></textarea>
-            <button className="send-button">Send</button>
-          </form>
-        </Slide>
-        <Slide direction="right">
+        <Slide direction="left">
           <div className="contact-detail">
             <div className="contact-sub">Let's talk how I can help you!</div>
             <div className="contact-message">
@@ -50,6 +56,30 @@ function Contact() {
               );
             })}
           </div>
+        </Slide>
+        <Slide direction="right" className="contact-form">
+          <form onSubmit={submitHandler}>
+            <label htmlFor="">Name</label>
+            <br />
+            <input ref={name} type="text" />
+            <br />
+            <label htmlFor="">Email</label>
+            <br />
+            <input ref={email} type="email" />
+            <br />
+            <label htmlFor="">Message</label>
+            <br />
+            <textarea
+              ref={message}
+              name=""
+              id=""
+              cols="30"
+              rows="12"
+            ></textarea>
+            <button type="submit" className="send-button">
+              Send
+            </button>
+          </form>
         </Slide>
       </div>
     </div>
