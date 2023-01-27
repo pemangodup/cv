588c192ecfab5ff891d57fc751efad00802b3cc4
Pema Ngodup <pngodup123@gmail.com>
2023-01-27 17:03:08 +0000

    api url error

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const variables = {
-  API_URI: 'http://localhost:5000/v1/cv/api',
-  // API_URI: 'https://colorful-hare-cape.cyclic.app/v1/cv/api',
+  // API_URI: 'http://localhost:5000/v1/cv/api',
+  API_URI: 'https://colorful-hare-cape.cyclic.app/v1/cv/api',
 };
 export default variables;
